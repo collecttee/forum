8bee1d554329d8c15400f0ae8ca95e342e3cb5e6
PC <PC@firedao.com>
2023-04-01 17:29:51 +0800

    s

---
+++ Sources/Point.php
@@ -0,0 +1,217 @@
+<?php
+function flm(){
+    $sa = isset($_GET['sa']) ? $_GET['sa'] : '';
+    $meritFunction = [
+        ''=>'flmtransfer',
+        'reward'=>'reward',
+        'internal'=>'internal',
+    ];
+    call_helper($meritFunction[$sa]);
+}
+function flmtransfer(){
+    global $scripturl, $context,$smcFunc,$user_info,$modSettings;
+    $context['post_url'] = $scripturl . '?action=profile;area=flm;save';
+    $request = $smcFunc['db_query']('', '
+                    SELECT  flm
+                    FROM {db_prefix}property
+                    WHERE id_member = {int:id_member}
+                    LIMIT 1',
+        array(
+            'id_member' => $user_info['id'],
+        )
+    );
+    $userProperty = $smcFunc['db_fetch_assoc']($request);
+    $flmAmount = $userProperty['flm'];
+    $context['flm']=$flmAmount;
+    if (isset($_SESSION['not-found']))
+    {
+        if ($_SESSION['not-found'] === true)
+            $context['not_found_user'] = true;
+        else
+            $context['saved_failed'] = $_SESSION['adm-save'];
+
+        unset($_SESSION['not-found']);
+    }
+    if (isset($_SESSION['adm-save']))
+    {
+        if ($_SESSION['adm-save'] === true)
+            $context['saved_successful'] = true;
+        else
+            $context['saved_failed'] = $_SESSION['adm-save'];
+
+        unset($_SESSION['adm-save']);
+    }
+    if (isset($_GET['save'])){
+        checkSession();
+        $amount = $_POST['amount'];
+        if ($flmAmount < $amount) {
+            fatal_error('Insufficient FLM quantity');
+        }
+        $username = $_POST['username'];
+        $request = $smcFunc['db_query']('', '
+			SELECT  address, id_member,  member_name
+			FROM {db_prefix}members
+			WHERE member_name = {string:username}
+			LIMIT 1',
+            array(
+                'username' => $username,
+            )
+        );
+        $user_settings = $smcFunc['db_fetch_assoc']($request);
+        if (empty($user_settings)){
+            $_SESSION['not-found'] = true;
+            redirectexit('action=profile;area=flm');
+        }
+        $request = $smcFunc['db_query']('', '
+			SELECT  flm
+			FROM {db_prefix}property
+			WHERE id_member = {int:id}
+			LIMIT 1',
+            array(
+                'id' => $user_settings['id_member'],
+            )
+        );
+        $toProperty = $smcFunc['db_fetch_assoc']($request);
+        $smcFunc['db_query']('', '
+					UPDATE {db_prefix}property
+					SET flm = {int:flm}
+					WHERE id_member = {int:id}',
+            array(
+                'flm' => $toProperty['flm'] + $amount,
+                'id' => $user_settings['id_member']
+            )
+        );
+        $smcFunc['db_query']('', '
+					UPDATE {db_prefix}property
+					SET flm = {int:flm}
+					WHERE id_member = {int:id}',
+            array(
+                'flm' => $flmAmount - $amount,
+                'id' => $user_info['id']
+            )
+        );
+        $smcFunc['db_insert']('',
+            '{db_prefix}property_transfer_log',
+            array(
+                'from' => 'int',
+                'to' => 'int',
+                'amount' => 'int',
+                'create_at' => 'int',
+                'pool' => 'int',
+                'property' => 'string',
+            ),
+            [$user_info['id'],$user_settings['id_member'],$amount,time(),1,'flm'],
+            array()
+        );
+        $_SESSION['adm-save'] = true;
+        redirectexit('action=profile;area=flm');
+    }
+
+
+    $request = $smcFunc['db_query']('', '
+			SELECT COUNT(*)
+			FROM {db_prefix}property_transfer_log as a WHERE property = {string:property} AND pool = {int:id} AND a.from = {int:user}',
+        array(
+            'user'=>$user_info['id'],
+            'id' => 1,
+            'property' => 'flm',
+        )
+    );
+    list ($context['num_members']) = $smcFunc['db_fetch_row']($request);
+    $smcFunc['db_free_result']($request);
+    $_REQUEST['start'] =  $_REQUEST['start']  ?? 0;
+    $context['page_index'] = constructPageIndex($scripturl . '?action=profile;area=flm', $_REQUEST['start'], $context['num_members'], $modSettings['defaultMaxMembers']);
+    $limit = $_REQUEST['start'];
+    $context['start'] = $_REQUEST['start'];
+    // member-lists
+    $request = $smcFunc['db_query']('', '
+				SELECT   sou.id as id,mem.member_name as a,mem2.member_name as b,mem2.pid,amount,create_at
+			FROM {db_prefix}property_transfer_log AS sou 
+				LEFT JOIN {db_prefix}members AS mem ON (sou.from = mem.id_member)
+				LEFT JOIN {db_prefix}members AS mem2 ON (sou.to = mem2.id_member)  WHERE property = {string:property} AND pool = {int:id} AND sou.from = {int:user} ORDER BY id DESC LIMIT {int:start}, {int:max}',
+        array(
+            'user'=>$user_info['id'],
+            'id' => 1,
+            'property' => 'flm',
+            'start' => $limit,
+            'max' => $modSettings['defaultMaxMembers'],
+        )
+    );
+    while ($row = $smcFunc['db_fetch_assoc']($request)) {
+        $context['users'][] = $row;
+    }
+}
+function reward(){
+    global $scripturl, $context,$smcFunc,$user_info,$modSettings;
+    $context['sub_template'] = 'reward';
+    $request = $smcFunc['db_query']('', '
+			SELECT COUNT(*)
+			FROM {db_prefix}property_transfer_log as a WHERE property = {string:property} AND pool = {int:id} AND a.to = {int:user}',
+        array(
+            'user'=>$user_info['id'],
+            'id' => 1,
+            'property' => 'sflm',
+        )
+    );
+    list ($context['num_members']) = $smcFunc['db_fetch_row']($request);
+    $smcFunc['db_free_result']($request);
+    $_REQUEST['start'] =  $_REQUEST['start']  ?? 0;
+    $context['page_index'] = constructPageIndex($scripturl . '?action=profile;area=flm', $_REQUEST['start'], $context['num_members'], $modSettings['defaultMaxMembers']);
+    $limit = $_REQUEST['start'];
+    $context['start'] = $_REQUEST['start'];
+    // member-lists
+    $request = $smcFunc['db_query']('', '
+				SELECT   sou.id as id,mem.member_name as a,mem2.member_name as b,mem.pid,amount,create_at
+			FROM {db_prefix}property_transfer_log AS sou 
+				LEFT JOIN {db_prefix}members AS mem ON (sou.from = mem.id_member)
+				LEFT JOIN {db_prefix}members AS mem2 ON (sou.to = mem2.id_member)  WHERE property = {string:property} AND pool = {int:id} AND sou.to = {int:user} ORDER BY id DESC LIMIT {int:start}, {int:max}',
+        array(
+            'user'=>$user_info['id'],
+            'id' => 1,
+            'property' => 'sflm',
+            'start' => $limit,
+            'max' => $modSettings['defaultMaxMembers'],
+        )
+    );
+    while ($row = $smcFunc['db_fetch_assoc']($request)) {
+        $context['users'][] = $row;
+    }
+}
+function internal(){
+    global $scripturl, $context,$smcFunc,$user_info,$modSettings;
+    $context['sub_template'] = 'internal';
+    $request = $smcFunc['db_query']('', '
+			SELECT COUNT(*)
+			FROM {db_prefix}property_transfer_log as a WHERE property = {string:property} AND pool = {int:id} AND a.to = {int:user}',
+        array(
+            'user'=>$user_info['id'],
+            'id' => 1,
+            'property' => 'flm',
+        )
+    );
+    list ($context['num_members']) = $smcFunc['db_fetch_row']($request);
+    $smcFunc['db_free_result']($request);
+    $_REQUEST['start'] =  $_REQUEST['start']  ?? 0;
+    $context['page_index'] = constructPageIndex($scripturl . '?action=profile;area=flm', $_REQUEST['start'], $context['num_members'], $modSettings['defaultMaxMembers']);
+    $limit = $_REQUEST['start'];
+    $context['start'] = $_REQUEST['start'];
+    // member-lists
+    $request = $smcFunc['db_query']('', '
+				SELECT   sou.id as id,mem.member_name as a,mem2.member_name as b,mem.pid,amount,create_at
+			FROM {db_prefix}property_transfer_log AS sou 
+				LEFT JOIN {db_prefix}members AS mem ON (sou.from = mem.id_member)
+				LEFT JOIN {db_prefix}members AS mem2 ON (sou.to = mem2.id_member)  WHERE property = {string:property} AND pool = {int:id} AND sou.to = {int:user} ORDER BY id DESC LIMIT {int:start}, {int:max}',
+        array(
+            'user'=>$user_info['id'],
+            'id' => 1,
+            'property' => 'flm',
+            'start' => $limit,
+            'max' => $modSettings['defaultMaxMembers'],
+        )
+    );
+    while ($row = $smcFunc['db_fetch_assoc']($request)) {
+        $context['users'][] = $row;
+    }
+}
+
+
